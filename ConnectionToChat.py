171fa3ff3b7f66439935951e6db0535f6cbe408a
_Smound_ <smoundmusic@gmail.com>
2023-04-05 11:34:39 -0600

    Move todo things form chat connection to readme

--- ConnectionToChat.py
+++ ConnectionToChat.py
@@ -34,23 +34,6 @@ for voice in voices:
 _global_file = ""
 _response_contains = []
 
-# TODO
-# El sistema entra por primera vez y valida que los archivos de datos existan
-# Basdo en el tipo de entrenamiento seleccionado el sistema seteará la variable "_global_file"
-# que indicará la ruta del archivo donde se tomarán los datos para entrenamiento
-# Basado en el entrenamiento, se pondrá un contexto referente a lo que se busca haga o responda
-# el asistente
-# Basado en el entrenamiento, debe setearse la variable "_response_contains" que nos indica que
-# posible respuesta o que puede contener el texto de respuesta para que actualicemos los datos
-# del entrenamiento
-# Casos de USO:
-# - Preguntas: El usuario realizará preguntas relacionadas con el tema en cuestion, calificando
-# la respuesta para poder agregarlas a los datos de entrenamiento.
-# - Respuestas: El asistente realizará preguntas sobre el tema en cuestion, calificando dichas
-# preguntas para que se añadan a la lista de preguntas permitidas.Siempre que el usuario así
-# lo determine
-
-
 def get_context_by_train(__entrenamiento_elegido):
 
     if config.trining_mode:
