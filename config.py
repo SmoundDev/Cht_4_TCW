84bf182edf30747369d81ed1d444555f4ad86907
_Smound_ <smoundmusic@gmail.com>
2023-04-11 19:09:16 -0600

    movimiento de configuraciones

--- config.py
+++
@@ -1,6 +0,0 @@
-
-api_key = "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"
-
-ai_name = "Spartan"
-
-trining_mode = False
---
+++ configurations/config.py
@@ -0,0 +1,11 @@
+
+api_key = "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"
+
+
+trining_mode = False
+
+class ai_variables:
+    
+    ai_name = "Spartan"
+
+    ai_surname = ["Yoel","Patan","Pale","Chris","Alberto","Nordix","Condor","Robert"]
